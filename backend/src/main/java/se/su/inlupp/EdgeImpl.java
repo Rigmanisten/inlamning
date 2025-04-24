a9f808e4be6d0ffad232cf611390d9cf945204f2
HentaiMaster444 <tigge06@gmail.com>
2025-04-24 14:44:52 +0200

    EdgeImpl implementerad WOW such cool

---
+++ backend/src/main/java/se/su/inlupp/EdgeImpl.java
@@ -0,0 +1,46 @@
+package se.su.inlupp;
+
+public class EdgeImpl<T> implements Edge<T> {
+    private T destination;
+    private String name;
+    private int weight;
+
+
+    public EdgeImpl(T destination, String name, int weight){
+        if (weight < 0) {
+            throw new IllegalArgumentException("weight must be posetive");
+        }
+
+
+        this.destination = destination;
+        this.name = name;
+        this.weight = weight;
+
+    }
+
+    @Override
+    public T getDestination() {
+        return destination;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        
+        return name + " " + destination + " " + weight;
+    }
+
+    @Override
+    public void setWeight(int weight) {
+        if (weight < 0) {
+            throw new IllegalArgumentException("Must be posetive");
+        }
+    }
+}
